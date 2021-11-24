de55850ff06b78d98a2a0ddfa25c026b2cafc315
minamihiroto <kokowatamf@gmail.com>
2021-11-24 13:33:54 +0900

    詳細ページのレイアウト調整

--- lib/show.dart
+++ lib/show.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class Show extends StatelessWidget {
+class Show extends StatefulWidget {
+  @override
+  _ShowState createState() => _ShowState();
+}
+
+class _ShowState extends State<Show> {
+  String text = 'エラー';
+  String citation = 'エラー';
+  int genre;
+  String genreMessage = 'エラー';
+
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -23,6 +33,44 @@ class Show extends StatelessWidget {
           body: SingleChildScrollView(
             child: Container(
               margin: EdgeInsets.only(right: 20, left: 20, top: 30),
+              child: Column(
+                children: [
+                  Center(
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: 240,
+                      margin: EdgeInsets.only(top: 10),
+                      child: SingleChildScrollView(
+                        child: Text(
+                          text.replaceAll('\\n', '\n'),
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.white,
+                            height: 2.75,
+                          ),
+                          textAlign: TextAlign.center,
+                          textHeightBehavior: TextHeightBehavior(
+                            applyHeightToFirstAscent: false,
+                            applyHeightToLastDescent: false,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 30),
+                    child: Text(
+                      '$genreMessage | $citation',
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.white70,
+                        fontStyle: FontStyle.italic,
+                      ),
+                      textAlign: TextAlign.right,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
