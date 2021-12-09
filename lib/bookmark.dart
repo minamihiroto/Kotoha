cd2424f516f94f2ddbca1b07862869c31b66d231
minamihiroto <kokowatamf@gmail.com>
2021-12-09 23:48:53 +0900

    重複しているブックマークを自分の端末から除外する機能の追加

--- lib/bookmark.dart
+++ lib/bookmark.dart
@@ -15,6 +15,7 @@ class _BookmarkState extends State<Bookmark> {
   Future reading() async {
     var box = await Hive.openBox('bookmark');
     final bookmark = List<String>.from(box.values);
+    final addedBookmarks = <String>[];
     await box.clear(); //一度全消し
     for (final docId in bookmark) {
       final ds = await FirebaseFirestore.instance
@@ -22,8 +23,12 @@ class _BookmarkState extends State<Bookmark> {
           .doc(docId)
           .get();
       if (ds.exists) {
-        mapBookmark.add(ds.data());
-        await box.add(ds.id); //あるものだけ追加
+        if (!addedBookmarks.contains(ds.id)) {
+          //重複しているもの以外を追加する
+          addedBookmarks.add(ds.id);
+          mapBookmark.add(ds.data());
+          await box.add(ds.id); //あるものだけ追加
+        }
       }
     }
     setState(() {});
@@ -53,131 +58,139 @@ class _BookmarkState extends State<Bookmark> {
     return Scaffold(
       // ボカシにしたい（ブラー？？）
       backgroundColor: Colors.black.withOpacity(0),
-      body: Center(
-        child: Container(
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                ...mapBookmark.map(
-                  //リストの中でリストを扱っているから...を使う
-                  (e) {
-                    final citation = e['citation'];
-                    final text = e['text'];
-                    final genre = e['genre'];
-                    setState(() {});
-                    return Container(
-                      margin: EdgeInsets.only(left: 20, right: 20),
-                      child: GestureDetector(
-                        onTap: () {
-                          // 画面遷移
-                          Navigator.of(context).push(
-                            PageRouteBuilder(
-                              opaque: false,
-                              pageBuilder: (BuildContext context, _, __) =>
-                                  Show(citation, text, switchBun(genre)),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            ...mapBookmark.map(
+              //リストの中でリストを扱っているから...を使う
+              (e) {
+                final citation = e['citation'];
+                final text = e['text'];
+                final genre = e['genre'];
+                final docId = e['id'];
+                setState(() {});
+                return Container(
+                  margin: EdgeInsets.only(left: 20, right: 20),
+                  child: GestureDetector(
+                    onTap: () {
+                      // 画面遷移
+                      Navigator.of(context).push(
+                        PageRouteBuilder(
+                          opaque: false,
+                          pageBuilder: (BuildContext context, _, __) =>
+                              Show(citation, text, switchBun(genre)),
+                        ),
+                      );
+                    },
+                    child: Container(
+                      color: Colors.transparent,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Container(
+                            margin: EdgeInsets.only(
+                                top: 30, left: 10, right: 10, bottom: 20),
+                            child: Center(
+                              child: Text(
+                                text,
+                                maxLines: 3,
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.white,
+                                  height: 2.25,
+                                ),
+                                textAlign: TextAlign.center,
+                                textHeightBehavior: TextHeightBehavior(
+                                  applyHeightToFirstAscent: false,
+                                  applyHeightToLastDescent: false,
+                                ),
+                              ),
                             ),
-                          );
-                        },
-                        child: Container(
-                          color: Colors.transparent,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.end,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
                             children: [
                               Container(
-                                margin: EdgeInsets.only(
-                                    top: 30, left: 10, right: 10, bottom: 20),
-                                child: Center(
-                                  child: Text(
-                                    text,
-                                    maxLines: 3,
-                                    overflow: TextOverflow.ellipsis,
-                                    style: TextStyle(
-                                      fontSize: 18,
-                                      color: Colors.white,
-                                      height: 2.25,
-                                    ),
-                                    textAlign: TextAlign.center,
-                                    textHeightBehavior: TextHeightBehavior(
-                                      applyHeightToFirstAscent: false,
-                                      applyHeightToLastDescent: false,
-                                    ),
+                                margin: EdgeInsets.only(right: 10),
+                                child: Text(
+                                  '${switchBun(genre)} | $citation',
+                                  maxLines: 3,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    color: Colors.white70,
+                                    height: 2.75,
+                                  ),
+                                  textAlign: TextAlign.center,
+                                  textHeightBehavior: TextHeightBehavior(
+                                    applyHeightToFirstAscent: false,
+                                    applyHeightToLastDescent: false,
                                   ),
                                 ),
                               ),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                children: [
-                                  Container(
-                                    margin: EdgeInsets.only(right: 10),
-                                    child: Text(
-                                      '${switchBun(genre)} | $citation',
-                                      maxLines: 3,
-                                      overflow: TextOverflow.ellipsis,
-                                      style: TextStyle(
-                                        fontSize: 14,
-                                        color: Colors.white70,
-                                        height: 2.75,
-                                      ),
-                                      textAlign: TextAlign.center,
-                                      textHeightBehavior: TextHeightBehavior(
-                                        applyHeightToFirstAscent: false,
-                                        applyHeightToLastDescent: false,
-                                      ),
-                                    ),
-                                  ),
-                                  Container(
-                                    child: GestureDetector(
-                                        onTap: () async {
-                                          var box =
-                                              await Hive.openBox('bookmark');
-                                          final listBookmark =
-                                              List<String>.from(box.values);
-                                          showDialog(
-                                            context: context,
-                                            builder: (_) =>
-                                                CupertinoAlertDialog(
-                                              content: Text(
-                                                "$text",
-                                                style: TextStyle(
-                                                  height: 1.5,
-                                                ),
-                                              ),
-                                              actions: [
-                                                CupertinoDialogAction(
-                                                  child: Text('ブックマークから外す'),
-                                                  onPressed: () {
-                                                    Navigator.of(context).pop();
-                                                  },
-                                                ),
-                                                CupertinoDialogAction(
-                                                  child: Text('キャンセル'),
-                                                  onPressed: () {
-                                                    Navigator.of(context).pop();
-                                                  },
-                                                ),
-                                              ],
+                              Container(
+                                child: GestureDetector(
+                                    onTap: () async {
+                                      var box = await Hive.openBox('bookmark');
+                                      final listBookmark =
+                                          List<String>.from(box.values);
+                                      showDialog(
+                                        context: context,
+                                        builder: (_) => CupertinoAlertDialog(
+                                          content: Text(
+                                            "$text",
+                                            style: TextStyle(
+                                              height: 1.5,
                                             ),
-                                          );
-                                        },
-                                        child: Icon(
-                                          Icons.delete_forever_rounded,
-                                          color: Colors.grey,
-                                        )),
-                                  ),
-                                ],
+                                          ),
+                                          actions: [
+                                            CupertinoDialogAction(
+                                              child: Text('ブックマークから外す'),
+                                              onPressed: () async {
+                                                await FirebaseFirestore.instance
+                                                    .collection('saying')
+                                                    .doc(docId)
+                                                    .update(
+                                                  {
+                                                    'bookmark':
+                                                        FieldValue.increment(-1)
+                                                  },
+                                                );
+                                                await box.delete(listBookmark
+                                                    .indexOf(docId));
+                                                Navigator.of(context).pop();
+                                                setState(() {});
+                                              },
+                                            ),
+                                            CupertinoDialogAction(
+                                              child: Text('キャンセル'),
+                                              onPressed: () {
+                                                Navigator.of(context).pop();
+                                              },
+                                            ),
+                                          ],
+                                        ),
+                                      );
+                                    },
+                                    child: Icon(
+                                      Icons.delete_forever_rounded,
+                                      color: Colors.grey,
+                                    )),
                               ),
-                              Divider(color: Colors.white54),
                             ],
                           ),
-                        ),
+                          Divider(color: Colors.white54),
+                        ],
                       ),
-                    );
-                  },
-                ).toList(),
-              ],
-            ),
-          ),
+                    ),
+                  ),
+                );
+              },
+            ).toList(),
+          ],
         ),
       ),
     );
